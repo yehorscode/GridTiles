6085330d43b2a5f89eb3cda997a09d1d42e68f08
Yehor <omelianenkoegor@gmail.com>
2025-11-08 12:37:50 +0100

    News tiles!

--- src/components/feed_tile.tsx
+++ src/components/feed_tile.tsx
@@ -1,22 +1,31 @@
 import RotatingTile from "@/components/rotatingCube/rotatingTile";
 import useFetchData from "./useFetchData";
-import { getFeedList } from "./useFetchData";
+
 export default function FeedTile() {
-  void console.log(useFetchData("world"));
+  const { feed, error } = useFetchData("europe");
+  void console.log(feed);
   return (
-    <RotatingTile>
-      <RotatingTile.Front>
-        <span>Feed Front</span>
-      </RotatingTile.Front>
-      <RotatingTile.Right>
-        <span>Feed Right</span>
-      </RotatingTile.Right>
-      <RotatingTile.Back>
-        <span>Feed Back</span>
-      </RotatingTile.Back>
-      <RotatingTile.Left>
-        <span>Feed Left</span>
-      </RotatingTile.Left>
-    </RotatingTile>
+    <div className="">
+      <RotatingTile border="transparent">
+        <RotatingTile.Front className="text-left !justify-end flex !items-end !bg-accent !border !border-white/30">
+          <span className="text-right text-xl">
+            {feed && <span>{feed?.items?.[1]?.title}</span>}
+          </span>
+        </RotatingTile.Front>
+        <RotatingTile.Right className="!p-0 !m-0 !border !border-white/30 !bg-accent">
+          <img src={feed?.items?.[1]?.image} alt="" />
+        </RotatingTile.Right>
+        <RotatingTile.Back className="text-left !justify-end flex !items-end !bg-accent !border !border-white/30">
+          <span className="text-right text-xl">
+            {feed && <span>{feed?.items?.[2]?.title}</span>}
+          </span>
+        </RotatingTile.Back>
+        <RotatingTile.Left className="!p-0 !m-0 !border !border-white/30 !bg-accent">
+          <img src={feed?.items?.[2]?.image} alt="" />
+        </RotatingTile.Left>
+      </RotatingTile>
+
+      <div className="mt-2 text-sm"></div>
+    </div>
   );
 }
